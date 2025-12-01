b67f7192b5950dc93dca96fd330f15a27429cb92
SushyDev <mail@sushy.dev>
2025-12-01 09:04:16 +0100

    fix plasma manager deps

--- flake.nix
+++ flake.nix
@@ -2,10 +2,15 @@
 	description = "A very basic flake";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-		determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3.11.3";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
 		chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable"; # IMPORTANT
-		plasma-manager.url = "github:nix-community/plasma-manager";
+
+		plasma-manager = {
+			url = "github:nix-community/plasma-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+			inputs.home-manager.follows = "home-manager";
+		};
 
 		nix-darwin = {
 			url = "github:nix-darwin/nix-darwin/master";
@@ -13,7 +18,7 @@
 		};
 
 		home-manager = {
-			url = "github:nix-community/home-manager";
+			url = "github:nix-community/home-manager/release-25.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
