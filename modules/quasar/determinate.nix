dcb82a34a34a1830d681fbedcde394264c4257e0
SushyDev <mail@sushy.dev>
2026-01-21 09:34:30 +0100

    comment builders

--- modules/quasar/determinate.nix
+++ modules/quasar/determinate.nix
@@ -6,12 +6,12 @@
 		experimental-features = "nix-command flakes external-builders";
 		trusted-users = setup.managedUsersAndRoot;
 		lazy-trees = true;
-		external-builders = builtins.toJSON [
-			{
-				systems = [ "x86_64-linux" "aarch64-linux" ];
-				program = "/usr/local/bin/darwin-nixd";
-				args = [ "builder" ];
-			}
-		];
+		# external-builders = builtins.toJSON [
+		# 	{
+		# 		systems = [ "x86_64-linux" "aarch64-linux" ];
+		# 		program = "/usr/local/bin/darwin-nixd";
+		# 		args = [ "builder" ];
+		# 	}
+		# ];
 	};
 }
