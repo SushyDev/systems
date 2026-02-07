9d01fea5b62640eff05b1411d1b31e291c4acd37
SushyDev <mail@sushy.dev>
2026-02-07 12:52:33 +0100

    fix: restore proper indentation in flake.nix

--- flake.nix
+++ flake.nix
@@ -57,85 +57,85 @@
 
 	outputs = { self, nixpkgs, disko, determinate, home-manager, plasma-manager, nix-darwin, nix-plist-manager, ... }@inputs:
 		let
-		systemPc = {
-			system = "x86_64-linux";
-			specialArgs = {
-				inherit inputs;
-				setup = {
-					primaryUser = "sushy";
-					managedUsers = [ systemPc.specialArgs.setup.primaryUser ];
-					managedUsersAndRoot = [ "root" ] ++ systemPc.specialArgs.setup.managedUsers;
-					nixGroupMembers = [ systemPc.specialArgs.setup.primaryUser ];
-					nixGroupName = "nix";
-					nixGroupId = 101;
-					systemFlakePath = "/etc/nixos";	
+			systemPc = {
+				system = "x86_64-linux";
+				specialArgs = {
+					inherit inputs;
+					setup = {
+						primaryUser = "sushy";
+						managedUsers = [ systemPc.specialArgs.setup.primaryUser ];
+						managedUsersAndRoot = [ "root" ] ++ systemPc.specialArgs.setup.managedUsers;
+						nixGroupMembers = [ systemPc.specialArgs.setup.primaryUser ];
+						nixGroupName = "nix";
+						nixGroupId = 101;
+						systemFlakePath = "/etc/nixos";	
+					};
 				};
+				modules = [
+					determinate.nixosModules.default
+					./modules/pc/configuration.nix
+
+					home-manager.nixosModules.home-manager
+					./modules/pc/home-manager.nix
+				];
 			};
-			modules = [
-				determinate.nixosModules.default
-				./modules/pc/configuration.nix
 
-				home-manager.nixosModules.home-manager
-				./modules/pc/home-manager.nix
-			];
-		};
-
-		systemQuasar = {
-			system = "aarch64-darwin";
-			specialArgs = {
-				inherit inputs;
-				setup = {
-					managedUsers = [ "sushy" "work" ];
-					managedUsersAndRoot = systemQuasar.specialArgs.setup.managedUsers ++ [ "root" ];
-					nixGroupName = "nix";
-					nixGroupId = 503;
-					systemFlakePath = "/private/etc/nixdarwin";
+			systemQuasar = {
+				system = "aarch64-darwin";
+				specialArgs = {
+					inherit inputs;
+					setup = {
+						managedUsers = [ "sushy" "work" ];
+						managedUsersAndRoot = systemQuasar.specialArgs.setup.managedUsers ++ [ "root" ];
+						nixGroupName = "nix";
+						nixGroupId = 503;
+						systemFlakePath = "/private/etc/nixdarwin";
+					};
 				};
+				modules = [
+					./modules/quasar/configuration.nix
+
+					determinate.darwinModules.default
+					./modules/quasar/determinate.nix
+
+					nix-plist-manager.darwinModules.default
+					./modules/quasar/plist-manager.nix
+
+					home-manager.darwinModules.home-manager
+					./modules/quasar/home-manager.nix
+				];
 			};
-			modules = [
-				./modules/quasar/configuration.nix
 
-				determinate.darwinModules.default
-				./modules/quasar/determinate.nix
-
-				nix-plist-manager.darwinModules.default
-				./modules/quasar/plist-manager.nix
-
-				home-manager.darwinModules.home-manager
-				./modules/quasar/home-manager.nix
-			];
-		};
-
-		systemPulsar = {
-			system = "x86_64-linux";
-			specialArgs = {
-				inherit inputs;
-				disko = disko;
-				setup = {
-					primaryUser = "sushy";
-					managedUsers = [ systemPulsar.specialArgs.setup.primaryUser ];
-					managedUsersAndRoot = [ "root" ] ++ systemPulsar.specialArgs.setup.managedUsers;
-					nixGroupMembers = [ systemPulsar.specialArgs.setup.primaryUser ];
-					nixGroupName = "nix";
-					nixGroupId = 101;
-					systemFlakePath = "/etc/nixos";	
+			systemPulsar = {
+				system = "x86_64-linux";
+				specialArgs = {
+					inherit inputs;
+					disko = disko;
+					setup = {
+						primaryUser = "sushy";
+						managedUsers = [ systemPulsar.specialArgs.setup.primaryUser ];
+						managedUsersAndRoot = [ "root" ] ++ systemPulsar.specialArgs.setup.managedUsers;
+						nixGroupMembers = [ systemPulsar.specialArgs.setup.primaryUser ];
+						nixGroupName = "nix";
+						nixGroupId = 101;
+						systemFlakePath = "/etc/nixos";	
+					};
 				};
+				modules = [
+					determinate.nixosModules.default
+					disko.nixosModules.disko
+					./modules/pulsar/disko/btrfs-raid1.nix
+
+					./modules/pulsar/configuration.nix
+
+					home-manager.nixosModules.home-manager
+					./modules/pulsar/home-manager.nix
+				];
 			};
-			modules = [
-				determinate.nixosModules.default
-				disko.nixosModules.disko
-				./modules/pulsar/disko/btrfs-raid1.nix
-
-				./modules/pulsar/configuration.nix
-
-				home-manager.nixosModules.home-manager
-				./modules/pulsar/home-manager.nix
-			];
+		in
+		{
+			nixosConfigurations.pc = nixpkgs.lib.nixosSystem systemPc;
+			darwinConfigurations.quasar = nix-darwin.lib.darwinSystem systemQuasar;
+			nixosConfigurations.pulsar = nixpkgs.lib.nixosSystem systemPulsar;
 		};
-	in
-	{
-		nixosConfigurations.pc = nixpkgs.lib.nixosSystem systemPc;
-		darwinConfigurations.quasar = nix-darwin.lib.darwinSystem systemQuasar;
-		nixosConfigurations.pulsar = nixpkgs.lib.nixosSystem systemPulsar;
-	};
 }
