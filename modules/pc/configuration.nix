e9196e4664a7a2d7d240f9b13fb94a7f7d591547
SushyDev <mail@sushy.dev>
2026-04-04 14:45:28 +0200

    Fix unitConfig - use direct memoryLimit and cpuQuota options

--- modules/pc/configuration.nix
+++ modules/pc/configuration.nix
@@ -18,6 +18,8 @@
     ../shared/fonts.nix
     ./desktop-manager/kde.nix
     # ./hardening.nix
+
+    inputs.m2-nix-container.nixosModules.default
   ];
 
   i18n.defaultLocale = "en_US.UTF-8";
@@ -28,5 +30,29 @@
   environment.shellAliases = {
     pbcopy = "wl-copy";
     pbpaste = "wl-paste";
+  };
+
+  # Magento 2 Setup - Appie Goossens Instance
+  services.magento = {
+    enable = true;
+    instances.appie-goossens = {
+      magentoSrcInput = "appie-goossens-m2";
+      dataPath = "/var/lib/containers/magento-appie";
+      networkPrefix = "192.168.100";
+
+      # Database
+      mariadb = {
+        host = "192.168.101.2";
+        port = 3306;
+        database = "magento_appie";
+        user = "magento";
+        password = "magento";
+        rootPassword = "magento";
+      };
+
+      # Resource limits
+      memoryLimit = "8G";
+      cpuQuota = "400%";
+    };
   };
 }
