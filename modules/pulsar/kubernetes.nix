7de10afe3785852972d5e5b4e02cb597425e7b6c
SushyDev <mail@sushy.dev>
2026-04-11 11:00:16 +0200

    k8s pulsar

--- modules/pulsar/kubernetes.nix
+++ modules/pulsar/kubernetes.nix
@@ -1,20 +1,45 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
 {
   services.kubernetes = {
     roles = ["master" "node"];
     masterAddress = config.networking.hostName;
-    easyCerts = true; # Automatically generates certificates
+    easyCerts = true;
     apiserverAddress = "https://${config.networking.hostName}:6443";
 
-    # Use CoreDNS for service discovery
     addons.dns.enable = true;
 
-    # Required if your server has swap enabled
     kubelet.extraOpts = "--fail-swap-on=false";
   };
 
-  # Make sure the master hostname resolves correctly
   networking.extraHosts = "127.0.0.1 ${config.networking.hostName}";
+
+  networking.firewall.trustedInterfaces = [
+    "mynet"
+    "cni0"
+    "flannel.1"
+  ];
+
+  systemd.services.flannel-rbac-patch = {
+    description = "Patch flannel ClusterRole to add get on nodes";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "kube-apiserver.service" ];
+    requires = [ "kube-apiserver.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 10";
+      ExecStart = pkgs.writeShellScript "flannel-rbac-patch" ''
+        export KUBECONFIG=/etc/kubernetes/cluster-admin.kubeconfig
+        until ${pkgs.kubectl}/bin/kubectl get clusterrole flannel &>/dev/null; do
+          echo "Waiting for flannel ClusterRole..."
+          sleep 5
+        done
+        ${pkgs.kubectl}/bin/kubectl patch clusterrole flannel --type=json \
+          -p='[{"op":"replace","path":"/rules/1/verbs","value":["get","list","watch"]}]'
+        echo "flannel ClusterRole patched."
+      '';
+    };
+  };
 
   # Standard packages for management
   environment.systemPackages = [
